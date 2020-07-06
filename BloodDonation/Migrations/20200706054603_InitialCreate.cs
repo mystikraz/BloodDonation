2061029d2e4da0cac81fdfceb39f74570db174bd
rajan <royal_raj@outlook.com>
2020-07-06 12:18:53 +0545

    Add project files.

---
+++ BloodDonation/Migrations/20200706054603_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodDonation.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Age = table.Column<string>(nullable: true),
+                    BloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidates");
+        }
+    }
+}
---
+++ BloodDonation/Models/DonationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonation.Models
+{
+    public class DonationDbContext : DbContext
+    {
+        public DonationDbContext(DbContextOptions<DonationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
